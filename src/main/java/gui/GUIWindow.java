50e723c56804c9fda6375dcf541be01ae1ae4633
cunningCoder <Lonelyx@yandex.ru>
2015-08-15 17:11:29 +0300

    0.2.1

--- src/main/java/gui/GUIWindow.java
+++ src/main/java/gui/GUIWindow.java
@@ -1,5 +1,42 @@
 package gui;
 
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
 public class GUIWindow {
+    public void mainFraime (){
+        JFrame frame = new JFrame("Калькулятор стоимости услуг ЖКХ");
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+
+        //panel for buttons
+        JPanel buttonPanel = new JPanel();
+        JButton readButton = new JButton("Получить данные");
+        JButton addButton = new JButton("Добавить данные");
+        JButton updateButton = new JButton("Обновить данные");
+        JButton deleteButton = new JButton("Удалить данные");
+        buttonPanel.add(readButton);
+        buttonPanel.add(addButton);
+        buttonPanel.add(updateButton);
+        buttonPanel.add(deleteButton);
+
+        //main panel for Jtable
+        JPanel mainPanel = new JPanel();
+
+
+        //info panel
+        JPanel infoPanel = new JPanel();
+        infoPanel.setBorder(new TitledBorder("Информация"));
+
+        frame.add(buttonPanel, BorderLayout.NORTH);
+        frame.add(mainPanel, BorderLayout.CENTER);
+        frame.add(infoPanel, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,7 +1,11 @@
 import crud.crud;
+import gui.GUIWindow;
 
 public class test {
     public static void main(String[] args)  {
+
+        GUIWindow guiWindow = new GUIWindow();
+        guiWindow.mainFraime();
 
         crud.createDb();
         crud.updateDb();
